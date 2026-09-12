6f3d85cef05032d5747a9e875d9511617dbf319b
ExtremeXT <extremext@lineageos.org>
2026-09-12 23:47:35 +0300

    x1s: Switch to 64bit libswdap library
    Change-Id: I4be87eaa441ac316c98630c59e3f9815094de5e3
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- x1s-vendor.mk
+++ x1s-vendor.mk
@@ -73,7 +73,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/x1s/proprietary/vendor/tee/driver/00000000-0000-0000-0000-53626f786476:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-53626f786476
 
 PRODUCT_PACKAGES += \
-    libswdap \
     camera.exynos990 \
     libbauthserver \
     libbauthtzcommon \
@@ -90,6 +89,7 @@ PRODUCT_PACKAGES += \
     libwrappergps \
     sensors.grip \
     sensors.sensorhub \
+    libswdap \
     HotwordEnrollmentOKGoogleEx3CORTEXM4 \
     HotwordEnrollmentXGoogleEx3CORTEXM4 \
     gpsd \
